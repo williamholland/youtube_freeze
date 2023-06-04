4f55fd61c1f9d8a1d3956a3aed3a2fb11d3b647b
Will Holland <will@willholland.me>
2023-06-04 23:38:13 +0700

    Update extension interface
    It now uses a more intuitive logic with only 1 button and no build in 30
    second limit

--- popup.js
+++ popup.js
@@ -2,34 +2,20 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 document.getElementById('mc_trigger').onclick = () => {
+    chrome.tabs.executeScript({ code: "var b = document.querySelector('video'); if(b){b.play();}" });
     let form = document.getElementById('secs_form');
+    let btn = document.getElementById('mc_trigger');
+    let prev = btn.innerHTML
+    btn.innerHTML = "Wait for it...";
 
     var startsIn = parseInt(form.secs.value);
-    console.log(startsIn);
+    var roundRange = getRndInteger(1000, startsIn*1000);
+    chrome.tabs.executeScript({ code: "console.log("+roundRange+");" });
 
-    var roundRange = getRndInteger(0, 30);
-
-    setTimeout(() => {
-        chrome.tabs.executeScript({ code: "var b = document.querySelector('button.ytp-play-button.ytp-button'); if(b){b.click();} console.log(() => {});" });
-    }, (startsIn + roundRange) * 1000);
-
-}
-
-document.getElementById('close').onclick = () => { window.close(); };
-
-document.getElementById('play_pause').onclick = () => {
-    chrome.tabs.executeScript({ code: "var b = document.querySelector('button.ytp-play-button.ytp-button'); if(b){b.click();}" });
-};
-
-document.getElementById('reset').onclick = () => {
-    let form = document.getElementById('secs_form');
-
-    var startsIn = parseInt(form.secs.value);
-    console.log(startsIn);
-
-    var roundRange = getRndInteger(0, 30);
 
     setTimeout(() => {
-        chrome.tabs.executeScript({ code: "var b = document.querySelector('button.ytp-play-button.ytp-button'); if(b){b.click();} console.log(() => {});" });
-    }, (startsIn + roundRange) * 1000);
+      btn.innerHTML = prev;
+      chrome.tabs.executeScript({ code: "var b = document.querySelector('video'); if(b){b.pause();}" });
+    }, roundRange);
+
 }
